7302070d63b2151b1441d591ad52ba291b16d375
MuhammadAbuzer <muhammadabuzer256@gmail.com>
2025-07-20 18:04:41 +0500

    Update cookie settings for production

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -11,8 +11,8 @@ const createTokenAndSetCookie = (res, user) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true, //
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
