6901b9b2e394117460019527395746252c14ecc6
MuhammadAbuzer <muhammadabuzer256@gmail.com>
2025-07-20 13:38:07 +0500

    fix: updated mongoose connection with async/await and removed deprecated options

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectingDb = async () => {
   try {
-    mongoose
+    await mongoose
       .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
       })
